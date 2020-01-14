e194a7e5cd7d276bfefcf716b85a2ac28aafc834
RamyaAC <ramya@arborgold.com>
2020-01-14 10:42:32 +0530

    unwanted code is removed in student component

--- src/app/student.component.ts
+++ src/app/student.component.ts
@@ -14,14 +14,10 @@ export class Student {
 export class StudentComponent {
   student = new Student();
   studentList = new Array<Student>();
-  // studentList = [[1, "ramya", 23], [2, "yamuna", 20], [3, "chandu", 25]];
 
-  // StudentId: number = 4;
-  // StudentName: string = "Ramya";
-  // StudentAge: number = 23;
   constructor() {}
   ngOnInit() {
-    // this.arrayfunction();
+    
     this.saveChanges(this.student)
   }
   saveChanges(student:Student)
